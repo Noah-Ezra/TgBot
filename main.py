2217db203df463226cd7f467e6f487a3a5792ce9
Noah Ezra <forworks12123@gmail.com>
2024-09-07 20:27:43 +0530

    Update main.py

--- main.py
+++ main.py
@@ -24,7 +24,9 @@ ANSHIF = Client(
 
 @ANSHIF.on_message(filters.command("start"))
 async def start_cmd(client, message):
-    ANSHIF.send_message(message.chat_id, 'test')
+    #ANSHIF.send_message(message.chat_id, 'test')
+    print(message)
+    print(client)
 
 @ANSHIF.on_message(filters.command("help"))
 async def help_cmd(client, message):
